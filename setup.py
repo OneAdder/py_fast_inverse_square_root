a3710b17cea51b29dc575dc89cc52bf4adb0c33c
OneAdder <mikivo@list.ru>
2021-01-03 13:12:41 +0300

    fix for setup

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
+import re
 from distutils.core import setup, Extension
-import fstinvsqrt
 
 FSTINVSQRT_MODULE = Extension('fstinvsqrt', sources = ['fstinvsqrt.c'])
 with open('README.md') as f:
     LONG_DESC = f.read()
+with open('fstinvsqrt.c') as f:
+    c_file = f.read()
+    DOC = re.findall(r'_MODULE_DOCSTRING\[\] = "(.*?)";', c_file)[0]
+    VERSION = re.findall(r'_VERSION\[\] = "(.*?)";', c_file)[0]
 
 
 setup(
     name='fstinvsqrt',
-    version=fstinvsqrt.__version__,
-    description=fstinvsqrt.__doc__,
+    version=VERSION,
+    description=DOC,
     long_description=LONG_DESC,
     long_description_content_type='text/markdown',
     author='Michael Voronov',
